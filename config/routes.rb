dda55701ea4d3c2a53edc9475c2bc8eedbfa7ca0
Alice Lara Rezende <hello@alicelara.ca>
2014-03-01 19:45:11 -0500

    Adding feature to delete pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 PhotogurRails4::Application.routes.draw do
-  root :to => "pictures#index"
   get 'pictures' => 'pictures#index'
-
+  
+  get 'pictures/:id' => 'pictures#show', as: "picture"
+  
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
-  get 'pictures/:id' => 'pictures#show', as: "picture"
-
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => 'pictures#update'
+
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
+  root :to => "pictures#index"
 end
